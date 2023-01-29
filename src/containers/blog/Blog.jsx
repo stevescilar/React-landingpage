b16d13df925d7d91eba8b4d4e015442ca5241eab
stevescilar <stevemuambi@gmail.com>
2023-01-29 10:27:32 +0300

    minor

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -18,7 +18,7 @@ const Blog = () => {
           </div>
           <div className="gpt3__blog-container_groupB">
             {/* group B */}
-            <Article imgUrl={blog02} date="Sep 26, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+            <Article imgUrl={blog02} date="Sep 26, 2023" title="GPT-3 and Open  AI is the future. Let us explore how it is?" />
             <Article imgUrl={blog03} date="Sep 26, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" /> 
             <Article imgUrl={blog04} date="Sep 26, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
             <Article imgUrl={blog05} date="Sep 26, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
